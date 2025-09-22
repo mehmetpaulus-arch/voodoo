5fb7384d8ca7a3bcd97aecc98e5100c37c3dc2de
mehmetpaulus <mehmetpaulus@example.com>
2025-09-22 13:17:24 +0200

    Fix Netlify build configuration - disable Next.js plugin

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  trailingSlash: true,
+  distDir: 'out',
+  // Disable Netlify Next.js plugin
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
 };
 
 module.exports = nextConfig;
